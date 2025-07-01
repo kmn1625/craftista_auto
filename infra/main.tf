8ff577292b8248ee7aa15f9e6b698239382f9624
kmn1625 <101081743+kmn1625@users.noreply.github.com>
2025-07-01 15:32:02 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,1 +1,18 @@
-##
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+    }
+  }
+
+  required_version = ">= 1.3"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
