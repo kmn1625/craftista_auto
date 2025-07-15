07ce892d66f2c85bad8a556498468d7c442d8d0b
kmn1625 <101081743+kmn1625@users.noreply.github.com>
2025-07-15 16:10:05 +0530

    Create OrigamiRepository.java

---
+++ voting/src/main/java/com/example/voting/repository/OrigamiRepository.java
@@ -0,0 +1,8 @@
+package com.example.voting.repository;
+
+import com.example.voting.model.Origami;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrigamiRepository extends JpaRepository<Origami, Long> {
+  int countByOrigamiId(Long origamiId);
+}
